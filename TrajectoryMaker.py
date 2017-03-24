fb9fec00fd90a4e62f69e01db52893eac8b45de0
Eyal W <eyalw711@gmail.com>
2017-03-24 15:55:33 +0300

    geopy: distance between coords, and generate dest from src,dist,bearing

--- TrajectoryMaker.py
+++ TrajectoryMaker.py
@@ -3,5 +3,17 @@
 Created on Fri Mar 24 14:10:18 2017
 
 @author: Eyal
-"""
+""" 
+from geopy.distance import vincenty, Point, VincentyDistance
 
+ks_p1 = (32.177916, 34.911915)
+ks_p2 = (32.177925, 34.911857)
+ptk_p1 = (32.101759, 34.850336)
+print(vincenty(ks_p1, ks_p2).meters)
+print(vincenty(ks_p1, ptk_p1).meters)
+
+ks_p3 = VincentyDistance(2).destination(ks_p1, 90)
+print(ks_p3[0], ks_p3[1])
+
+
+    
